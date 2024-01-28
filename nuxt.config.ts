8d7dc7ea8c14a1aaa9aec82bd65ed3c1b5f157a8
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2024-01-28 23:15:51 +0100

    ogimage test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: true,
   site: {
-    url: 'https://www.kristinalinhartova.com/',
+    url: 'https://klinhartova-portfolio.vercel.app/',
   },
   app: {
     head: {
